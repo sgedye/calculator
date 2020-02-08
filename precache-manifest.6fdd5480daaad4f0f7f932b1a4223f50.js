ef3fd683319b929910b640adf6b2bf24ea85536c
Shaun Gedye <sgedye88@yahoo.com.au>
2020-02-08 23:21:50 +1100

    Updates

---
+++ precache-manifest.6fdd5480daaad4f0f7f932b1a4223f50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e94468ffc9aaecffea77f07cfe94a1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c14849d9975dd9b0583e",
+    "url": "/calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "35e4af530d87861c7198",
+    "url": "/calculator/static/js/2.919bf8b1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.919bf8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14849d9975dd9b0583e",
+    "url": "/calculator/static/js/main.5a3b3099.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
